1813adccb8dfa7b1082fd19b7b05ca31d1761ba4
jaw1005 <jaw1005@sru.edu>
2014-04-30 18:39:17 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,11 +1,6 @@
 //Separate js file for the about, for cleanliness
 
 function about(){
-   var newWindow = window.open("", "pictureViewer", "fullscreen=no","bgcolor='white'");// sets a new window opening
-   
-   newWindow.document.write("<html><body>)
-	 newWindow.document.write("<h1><center>About</center></h1>")
-	 
-	 newWindow.document.write("</body></html>")
+ alert("about")
 
 }
