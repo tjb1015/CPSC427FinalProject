6396d2016d4c63feeaa68efe5e26531667501a5e
jaw1005 <jaw1005@sru.edu>
2014-04-30 18:34:19 -0400

    Create about.js

---
+++ js/about.js
@@ -0,0 +1,11 @@
+//Separate js file for the about, for cleanliness
+
+function about(){
+   var newWindow = window.open("", "pictureViewer", "fullscreen=no","bgcolor='white'");// sets a new window opening
+   
+   newWindow.document.write("<html><body>)
+	 newWindow.document.write("<h1><center>About</center></h1>")
+	 
+	 newWindow.document.write("</body></html>")
+
+}
